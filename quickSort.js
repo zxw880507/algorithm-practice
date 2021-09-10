cf9624a89a0f22a3001a397d546002b7b8674371
Calvin <zxw880507@gmail.com>
2021-09-09 18:19:05 -0600

    pivot sort

---
+++ quickSort.js
@@ -0,0 +1,44 @@
+/* Implement Quick Sort
+
+Here we will move on to an intermediate sorting algorithm:
+ quick sort. Quick sort is an efficient, recursive divide-and-conquer approach to sorting an array. 
+ In this method, a pivot value is chosen in the original array. 
+ The array is then partitioned into two subarrays of values less than and 
+ greater than the pivot value. 
+ We then combine the result of recursively calling the quick sort algorithm on both sub-arrays. 
+ This continues until the base case of an empty or single-item array is reached, which we return. 
+ The unwinding of the recursive calls return us the sorted array.
+
+Quick sort is a very efficient sorting method, 
+providing O(nlog(n)) performance on average. 
+It is also relatively easy to implement. 
+These attributes make it a popular and useful sorting method.
+
+Instructions: Write a function quickSort which takes an array of integers as 
+input and returns an array of these integers in sorted order from least to greatest. 
+While the choice of the pivot value is important, 
+any pivot will do for our purposes here. 
+For simplicity, the first or last element could be used. */
+
+function quickSort(array) {
+  // Only change code below this line
+  const helper = (arr) => {
+    if (arr.length < 2) return arr;
+    let lower = [];
+    let equal = [];
+    let greater = [];
+    let pivot = arr[arr.length - 1];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        lower.push(arr[i]);
+      } else if (arr[i] > pivot) {
+        greater.push(arr[i]);
+      } else {
+        equal.push(arr[i]);
+      }
+    }
+    return [...helper(lower), ...equal, ...helper(greater)];
+  };
+  return helper(array);
+  // Only change code above this line
+}
